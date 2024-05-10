760c077e1e2b4098c94ded9b6e50910390f5b327
Giorgio Liapakis <giorgioliapakis@gmail.com>
2024-05-10 19:52:23 +1000

    removed branding

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -44,17 +44,17 @@ export const Badge = (props: Props) => {
         'background-color': props.badgeBackgroundColor ?? '#ffffff',
       }}
     >
-      Powered by
+      made with ❤️ by
       <a
         ref={liteBadge}
-        href={'https://flowiseai.com'}
+        href={'https://giorgio.co'}
         target="_blank"
         rel="noopener noreferrer"
         class="lite-badge"
         id="lite-badge"
         style={{ 'font-weight': 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> Flowise</span>
+        <span> giorgio</span>
       </a>
     </span>
   );
